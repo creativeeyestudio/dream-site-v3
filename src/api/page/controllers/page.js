fb024d2f94c7ef9bd31889af1d9ab203f8b6a9c5
creativeeyestudio <contact@creative-eye.fr>
2025-05-18 10:43:54 +0200

    Page API

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -6,65 +6,67 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-const api = 'api::page.page';
+module.exports = createCoreController('api::page.page');
 
-module.exports = createCoreController(api, ({ strapi }) => ({
-  async find(ctx) {
-    const entities = await initQuery(strapi).findMany();
+// const api = 'api::page.page';
 
-    return !entities  
-      ? ctx.notFound('Pages not found')
-      : ctx.body = entities;
-  },
+// module.exports = createCoreController(api, ({ strapi }) => ({
+//   async find(ctx) {
+//     const entities = await initQuery(strapi).findMany();
 
-  async findByHomepage(ctx) {
-    const entity = await initEntity(strapi, { homepage: true })
+//     return !entities  
+//       ? ctx.notFound('Pages not found')
+//       : ctx.body = entities;
+//   },
 
-    if (!entity) return ctx.notFound('Page not found');
+//   async findByHomepage(ctx) {
+//     const entity = await initEntity(strapi, { homepage: true })
 
-    const detailedEntity = await strapi.entityService.findOne(api, entity.id, {
-      populate: populate(),
-    });
+//     if (!entity) return ctx.notFound('Page not found');
 
-    // @ts-ignore
-    return this.transformResponse(detailedEntity).data;
-  },
+//     const detailedEntity = await strapi.entityService.findOne(api, entity.id, {
+//       populate: populate(),
+//     });
+
+//     // @ts-ignore
+//     return this.transformResponse(detailedEntity).data;
+//   },
   
-  async findBySlug(ctx) {
-    const { slug } = ctx.params;
-    const entity = await initEntity(strapi, { slug });
+//   async findBySlug(ctx) {
+//     const { slug } = ctx.params;
+//     const entity = await initEntity(strapi, { slug });
 
-    if (!entity) return ctx.notFound('Page not found');
+//     if (!entity) return ctx.notFound('Page not found');
 
-    const detailedEntity = await strapi.entityService.findOne(api, entity.id, {
-      populate: populate(),
-    });
+//     const detailedEntity = await strapi.entityService.findOne(api, entity.id, {
+//       populate: populate(),
+//     });
 
-    // @ts-ignore
-    return this.transformResponse(detailedEntity).data;
-  },
-}));
+//     // @ts-ignore
+//     return this.transformResponse(detailedEntity).data;
+//   },
+// }));
 
-function initQuery(strapi) {
-  return strapi.db.query(api);
-}
+// function initQuery(strapi) {
+//   return strapi.db.query(api);
+// }
 
-function initEntity(strapi, where) {
-  return initQuery(strapi).findOne({
-    where: where,
-  })
-}
+// function initEntity(strapi, where) {
+//   return initQuery(strapi).findOne({
+//     where: where,
+//   })
+// }
 
-function populate() {
-  return {
-    seo: {},
-    content_page: {
-      on: {
-        'page.text-image': { populate: ['image'] },
-        'page.text-double-image': { populate: ['image1', 'image2'] },
-        'page.carousel': { populate: ['images'] },
-        'page.parallax': { populate: ['image'] },
-      },
-    },
-  }
-}
+// function populate() {
+//   return {
+//     seo: {},
+//     content_page: {
+//       on: {
+//         'page.text-image': { populate: ['image'] },
+//         'page.text-double-image': { populate: ['image1', 'image2'] },
+//         'page.carousel': { populate: ['images'] },
+//         'page.parallax': { populate: ['image'] },
+//       },
+//     },
+//   }
+// }
